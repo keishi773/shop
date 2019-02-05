f3181af45d337b4feae7e6df07cadf9e59df47ae
keishi773 <keishi777@hotmail.co.jp>
2019-02-06 06:29:50 +0900

    2gatu6hi

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -25,10 +25,24 @@
                     <a class="navbar-brand" href="{{ url('/') }}">
                         {{ config('app.name', 'Laravel') }}
                     </a>
-                    <button class="navbar-toggler-icon"
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <!-- 左のサイドバー -->
+                        <ul class="navbar-nav ml-auto">
+                        </ul>
+                        <!-- 右のサイドバー -->
+                        <ul class="navbar-nav ml-auto">
+                        </ul>
+                    </div>
 
                 </div>
             </nav>
+            <!-- ここまでナビゲーションバー -->
+            <main content
         </div>
     </body>
 
