63193ad034227e6c90df4883b3bdc5d1af6d21f9
keishi773 <keishi777@hotmail.co.jp>
2019-02-06 05:59:10 +0900

    2019/2/6

---
+++ app/Http/Controllers/Admin/ShopController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ShopController extends Controller
+{
+    public function add(){
+        return view('admin.shop.create');
+    }
+
+    public function create(){
+
+        return redirect('admin/shop/create');
+    }
+
+
+}
---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}" >
+        <title>@yield('title')</title>
+
+        <!-- Script -->
+        <script src="{{ asset('js/app.js') }}" defer></script>
+
+        <!-- Fonts -->
+        <link rel="dns-prefetch" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
+
+        <!-- style -->
+        <link href="{{ asset('css/app.css')}}" rel="stylesheet">
+        <link href="{{ asset('css/admin.css') }}" rel="stylesheet">
+    </head>
+    <body>
+        <div id="app">
+            <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
+                <div class="container">
+                    <a class="navbar-brand" href="{{ url('/') }}">
+                        {{ config('app.name', 'Laravel') }}
+                    </a>
+                    <button class="navbar-toggler-icon"
+
+                </div>
+            </nav>
+        </div>
+    </body>
+
+</html>
